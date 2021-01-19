a4b6b5642d4d9c114fa97c19ad7722d5315a4499
Vilmir Baikov <vilmir.baikoff@yandex.ru>
2021-01-19 21:57:57 +0500

    slider ui is good now

--- js.js
+++ js.js
@@ -4,11 +4,14 @@ class Observer {
     }
 }
 class Model {
-    constructor(min, max, val) {
+    constructor(min, max, val, step) {
         this.min = min
         this.max = max
         this.val = val
         this.step = step
+    }
+    getDiff = () => {
+        return this.max - this.min
     }
     getMax = () => {
         return this.max
@@ -47,20 +50,40 @@ class Controller {
         this.hand = this.view.handler
         this.mainAbsX = this.view.main[0].offsetLeft
         this.handWidth = this.hand[0].offsetWidth
+        this.rangeWidth = this.view.main[0].offsetWidth
         //this.currentVal = this.model.getVal()
         //this.max = this.model.getMax()
         //this.min = this.model.getMin()
     }
-    currentvalToPercent() {
-        return (this.model.getVal() / this.model.getMax())*100
+
+    currentValToPx(px) {
+        return (px/this.model.getMax())*this.rangeWidth
+    }
+    currentPxToVal(px) {
+        return (px/this.rangeWidth)*this.model.getMax()
+    }
+    getNumForStep(num, step, max) {
+        let res = Math.floor(num/step)*step
+        if (res >= max) res = max
+        return res
     }
     bind() {
-        console.log(this.currentvalToPercent())
-        this.hand.css({'left' : `${this.currentvalToPercent()}%`})
+        this.hand.css({'left' : `${10}px`})
+        let handDiff = Math.floor(this.handWidth/2)
         this.hand.on('mousedown', (e) => {
             $('html').on('mousemove', (event) => {
-                this.currentVal = event.pageX - this.mainAbsX - (this.handWidth/2)
-                this.hand.css({'left' : `${this.currentvalToPercent()}%`})
+                let px = event.pageX - this.mainAbsX - handDiff
+                let modelVal = this.currentPxToVal(px)
+                this.model.setVal(modelVal)
+
+                let pxForRange = this.currentValToPx(this.model.getVal())
+
+                /* if (pxForRange > (this.rangeWidth - this.handWidth)) {
+                    pxForRange = this.rangeWidth - this.handWidth
+                } */
+                //let currentVal = event.pageX - this.mainAbsX - handDiff
+                console.log(this.model.getVal())
+                this.hand.css({'left' : `${pxForRange}px`})
             })
         })
         $('html').on('mouseup', () => {
@@ -82,7 +105,7 @@ class View {
 
 
 $(document).ready(() => {
-    let model = new Model(0, 200, 123)
+    let model = new Model(0, 100, 10, 10)
     let view = new View()
     let cont = new Controller(model, view)
     cont.bind()
